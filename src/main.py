a5f6236df3e0066bc662fc7525c651b2ae40746a
Mohamad Arsya Kaukabi <345779@corp.bri.co.id>
2024-12-26 16:04:22 +0700

    debug struct

--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ from byaldi import RAGMultiModalModel
 RAG = RAGMultiModalModel.from_index("../byaldi/lkpp-multimodal")
 
 def handler(job):
-    query = job["query"]
+    job_input = job["input"]
+    query = job_input["query"]
 
     results = RAG.search(query=query, k=5)
 
